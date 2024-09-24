0666d1daf7f489f8118f40fa53e6d15f02152509
sanskruti019 <sanskrutikakad019@gmail.com>
2024-09-24 16:31:27 +0530

    add publishable key

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react';
 
-const PUBLISHABLE_KEY =import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY : string = "pk_test_YWJsZS10dW5hLTU3LmNsZXJrLmFjY291bnRzLmRldiQ";
 if(!PUBLISHABLE_KEY){
   throw new Error("Missing Publishable Key");
 }
